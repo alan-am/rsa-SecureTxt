cf3205218be634d727b9fc3009b59d040abd5988
alan-am <alan.aguilardev@gmail.com>
2025-08-17 11:20:00 -0500

    Add springboot base

---
+++ src/main/java/com/espol/EspolApplication.java
@@ -0,0 +1,13 @@
+package com.espol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espol/EspolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
