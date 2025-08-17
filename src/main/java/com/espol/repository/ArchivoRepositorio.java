27443182bf695ffcaea9de5e97df362813a6f005
alan-am <alan.aguilardev@gmail.com>
2025-08-17 14:44:18 -0500

    Add repositorios

---
+++ src/main/java/com/espol/repository/ArchivoRepositorio.java
@@ -0,0 +1,7 @@
+package com.espol.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArchivoRepositorio extends JpaRepository<ArchivoRepositorio, Long> {
+
+}
