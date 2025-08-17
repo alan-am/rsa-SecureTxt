06cc97b39cd7ff94a5e0258a89a497ce46e4f0af
alan-am <alan.aguilardev@gmail.com>
2025-08-17 18:01:53 -0500

    Fix confusion entidad Archivo en repo

--- src/main/java/com/espol/repository/ArchivoRepositorio.java
+++ src/main/java/com/espol/repository/ArchivoRepositorio.java
@@ -2,6 +2,8 @@ package com.espol.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArchivoRepositorio extends JpaRepository<ArchivoRepositorio, Long> {
+import com.espol.entity.RegistroArchivo;
+
+public interface ArchivoRepositorio extends JpaRepository<RegistroArchivo, Long> {
 
 }
