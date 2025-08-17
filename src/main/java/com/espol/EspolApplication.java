429b39bfdb769b35f2ed25eae2ba188a6561631c
alan-am <alan.aguilardev@gmail.com>
2025-08-17 12:28:21 -0500

    Add entity Usuario

--- src/main/java/com/espol/EspolApplication.java
+++ src/main/java/com/espol/EspolApplication.java
@@ -1,13 +1,22 @@
 package com.espol;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EspolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EspolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Cargar variables de entorno desde .env
+        Dotenv dotenv = Dotenv.load();
 
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(EspolApplication.class, args);
+    }
 }
---
+++ src/main/java/com/espol/entity/Usuario.java
@@ -0,0 +1,38 @@
+package com.espol.entity;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    // Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "clavePublica", nullable = false)
+    private String clavePublica;
+
+    @Column(name = "clavePrivada", nullable = false)
+    private String clavePrivada;
+
+
+    
+}
